b4e5b56790be1287ff3fdfb6b5b702762e49d9a1
Daniel Everland <danieleverland@gmail.com>
2018-03-13 01:16:49 +0100

    Implemented texture atlas

---
+++ Assets/Scripts/MapGeneration/Atlas.cs
@@ -0,0 +1,49 @@
+﻿using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Atlas.asset", menuName = "Game/Atlas", order = 69)]
+public class Atlas : ScriptableObject {
+    
+    public Texture2D Texture
+    {
+        get
+        {
+            if (_texture == null)
+                CreateTexture();
+
+            return _texture;
+        }
+    }
+    private Texture2D _texture;
+
+    private Dictionary<TileType, Rect> _tileRectangles;
+
+    [SerializeField]
+    private List<TileType> _tiles;
+    [SerializeField]
+    private int _padding = 1;
+    [SerializeField]
+    private int _maxSize = 4096;
+
+    public Rect GetRect(TileType tileType)
+    {
+        if (_tileRectangles == null)
+            CreateTexture();
+
+        return _tileRectangles[tileType];
+    }
+    private void CreateTexture()
+    {
+        _tileRectangles = new Dictionary<TileType, Rect>();
+
+        _texture = new Texture2D(_maxSize, _maxSize);
+        Rect[] uvs = _texture.PackTextures(_tiles.Select(x => x.Texture).ToArray(), _padding, _maxSize);
+
+        for (int i = 0; i < uvs.Length; i++)
+        {
+            _tileRectangles.Add(_tiles[i], uvs[i]);
+        }
+    }
+}
---
+++ Assets/Scripts/MapGeneration/TileType.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Tile.asset", menuName = "Game/Tile", order = 69)]
+public class TileType : ScriptableObject {
+
+    public Sprite Sprite
+    {
+        get
+        {
+            return _sprite;
+        }
+    }
+    public int Width
+    {
+        get
+        {
+            return (int)_sprite.textureRect.x;
+        }
+    }
+    public int Height
+    {
+        get
+        {
+            return (int)_sprite.textureRect.y;
+        }
+    }
+    public Vector2 TextureOffset
+    {
+        get
+        {
+            return _sprite.textureRectOffset;
+        }
+    }
+    public Texture2D Texture
+    {
+        get
+        {
+            if (_texture == null)
+                CreateTexture();
+
+            return _texture;
+        }
+    }
+
+    private Texture2D _texture;
+
+    [SerializeField]
+    private Sprite _sprite;
+
+    public void CreateTexture()
+    {
+        _texture = new Texture2D(Width, Height);
+
+        Color[] pixels = _sprite.texture.GetPixels((int)TextureOffset.x, (int)TextureOffset.y, Width, Height);
+
+        _texture.SetPixels(pixels);
+        _texture.Apply();
+    }
+}
