ce06d527ad9756333fafacbdf1927d945a196ae4
Daniel Everland <danieleverland@gmail.com>
2018-03-15 23:43:29 +0100

    Added different lighting depending on the time of day

---
+++ Assets/Scripts/Day Cycle/Sun.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Light))]
+public class Sun : MonoBehaviour {
+
+    [SerializeField]
+    private Light _light;
+    [SerializeField]
+    private Gradient _ambientColor;
+    [SerializeField]
+    private AnimationCurve _angle;
+
+    private void OnValidate()
+    {
+        _light = GetComponent<Light>();
+    }
+    private void Update()
+    {
+        AnimateColor();
+    }
+    private void AnimateColor()
+    {
+        RenderSettings.ambientLight = _ambientColor.Evaluate(DayCycle.DayPercentage);
+    }
+}
