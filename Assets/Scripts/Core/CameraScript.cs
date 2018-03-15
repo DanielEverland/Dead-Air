e948adfef80b501a26dcf2f8d1b41138c2f7a380
Daniel Everland <danieleverland@gmail.com>
2018-03-15 19:57:28 +0100

    Implemented z-depth to camera

--- Assets/Scripts/Core/CameraScript.cs
+++ Assets/Scripts/Core/CameraScript.cs
@@ -6,6 +6,8 @@ public class CameraScript : MonoBehaviour {
 
     [SerializeField]
     private float _speed;
+    [SerializeField]
+    private float _zDepth;
     
     private void Update()
     {
@@ -13,6 +15,9 @@ public class CameraScript : MonoBehaviour {
     }
     private void Move()
     {
-        transform.position = Vector2.Lerp(transform.position, ColonistManager.SelectedColonist.transform.position, _speed * Time.deltaTime);
+        Vector3 newPos = Vector2.Lerp(transform.position, ColonistManager.SelectedColonist.transform.position, _speed * Time.deltaTime);
+        newPos.z = _zDepth;
+
+        transform.position = newPos;
     }
 }
