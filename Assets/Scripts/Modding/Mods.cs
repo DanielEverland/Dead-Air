e6bd8cf7ec09f5ab3742af50afb979ea3e797cd7
Daniel Everland <danieleverland@gmail.com>
2018-03-30 16:27:29 +0200

    Re-implemented mod loading

--- Assets/Scripts/Modding/Mods.cs
+++ Assets/Scripts/Modding/Mods.cs
@@ -2,65 +2,59 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UMS;
+
+#if UNITY_EDITOR
+using UMS.Editor;
+#endif
 
 public static class Mods {
 
-    //public static IEnumerable<Object> Objects { get { return Deserializer.Objects.Values; } }
-    //public static IEnumerable<string> Keys { get { return Deserializer.Objects.Keys; } }
-    //public static IDictionary<string, Object> Entries { get { return Deserializer.Objects; } }
+    public static IEnumerable<object> Objects { get { return ObjectContainer.Objects; } }
+    public static IEnumerable<string> Keys { get { return ObjectContainer.Keys; } }
 
     public static void Deserialize()
     {
-//        Deserializer.Initialize();
-
-//#if UNITY_EDITOR
-//        EditorSession.Load();
-//#else
-//        DeserializeBuiltGame();
-//#endif
+#if UNITY_EDITOR
+        EditorSession.Load();
+#else
+        DeserializeBuiltGame();
+#endif
     }
     private static void DeserializeBuiltGame()
     {
-        //Queue<string> directoryQueue = new Queue<string>();
-        //directoryQueue.Enqueue(Application.dataPath);
+        Queue<string> directoryQueue = new Queue<string>();
+        directoryQueue.Enqueue(Application.dataPath);
 
-        //while (directoryQueue.Count > 0)
-        //{
-        //    string currentFolder = directoryQueue.Dequeue();
+        while (directoryQueue.Count > 0)
+        {
+            string currentFolder = directoryQueue.Dequeue();
 
-        //    foreach (string file in Directory.GetFiles(currentFolder))
-        //    {
-        //        if (Path.GetExtension(file) == ".mod")
-        //        {
-        //            Deserializer.DeserializePackage(file);
-        //        }
-        //    }
+            foreach (string file in Directory.GetFiles(currentFolder))
+            {
+                if (Path.GetExtension(file) == ".mod")
+                {
+                    UMS.Mods.Load(file);
+                }
+            }
 
-        //    foreach (string subfolder in Directory.GetDirectories(currentFolder))
-        //    {
-        //        directoryQueue.Enqueue(subfolder);
-        //    }
-        //}
+            foreach (string subfolder in Directory.GetDirectories(currentFolder))
+            {
+                directoryQueue.Enqueue(subfolder);
+            }
+        }
     }
     public static bool Contains(string key)
     {
-        throw new System.NotImplementedException();
-        //if (!Deserializer.HasDeserialized)
-        //    PollDeserializer();
-
-        //return Deserializer.KeyExists(key);
+        return ObjectContainer.ContainsKey(key);
     }
     public static T GetObject<T>(string key) where T : Object
     {
         return (T)GetObject(key);
     }
-    public static Object GetObject(string key)
+    public static object GetObject(string key)
     {
-        throw new System.NotImplementedException();
-        //if (!Deserializer.HasDeserialized)
-        //    PollDeserializer();
-
-        //return Deserializer.GetObject(key);
+        return ObjectContainer.GetObjectFromKey(key);
     }
     private static void PollDeserializer()
     {
