d9aa644b0d89ba10a838730d0aa85b25c8da8bfe
Daniel Everland <danieleverland@gmail.com>
2018-05-11 15:37:48 +0200

    Added menu items for opening up data paths

---
+++ Dead-Air/Assets/Scripts/Editor/OpenDirectoryMenu.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Diagnostics;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public static class OpenDirectoryMenu {
+
+    private const int PRIORITY = 10000;
+
+    [MenuItem("Window/Open Editor Data", priority = PRIORITY)]
+    private static void OpenEditorData()
+    {
+        Process.Start(Directories.EditorDataPath);
+    }
+    [MenuItem("Window/Open Build Data", priority = PRIORITY)]
+    private static void OpenBuildData()
+    {
+        string fullPathOfExe = EditorUserBuildSettings.GetBuildLocation(EditorUserBuildSettings.activeBuildTarget);
+        string appDirectory = Path.GetDirectoryName(fullPathOfExe);
+        string dataPath = $"{appDirectory}/{Path.GetFileNameWithoutExtension(fullPathOfExe)}_Data";
+        
+        Process.Start(dataPath);
+    }
+}
