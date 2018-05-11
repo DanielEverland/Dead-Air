fc1dc3a7e6786ed35c9a26e7179375062c100f6d
Daniel Everland <danieleverland@gmail.com>
2018-05-11 15:06:51 +0200

    Restructured scenes

---
+++ Dead-Air/Assets/Scripts/Utility/Utility.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+public static class Utility {
+
+    public static bool SceneLoaded(string name)
+    {
+        for (int i = 0; i < SceneManager.sceneCount; i++)
+        {
+            if (SceneManager.GetSceneAt(i).name == name)
+                return true;
+        }
+
+        return false;
+    }
+}
