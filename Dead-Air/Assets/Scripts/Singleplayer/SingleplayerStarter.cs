3952b0d07ca2b76ef9884a10ef25d3a57d01ff13
Daniel Everland <danieleverland@gmail.com>
2018-05-12 14:03:12 +0200

    Added singleplayer scene

---
+++ Dead-Air/Assets/Scripts/Singleplayer/SingleplayerStarter.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SingleplayerStarter : MonoBehaviour {
+
+    private void Awake()
+    {
+        SceneManager.LoadScene("Main", LoadSceneMode.Single);
+        SceneManager.LoadScene("Server", LoadSceneMode.Additive);
+    }
+}
