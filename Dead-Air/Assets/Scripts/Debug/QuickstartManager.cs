a32dafa6f41a11b7e90cdfd810e31e41a957e03d
Daniel Everland <danieleverland@gmail.com>
2018-05-12 13:45:42 +0200

    Added a quickstart scene

---
+++ Dead-Air/Assets/Scripts/Debug/QuickstartManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class QuickstartManager : MonoBehaviour {
+
+    private void Awake()
+    {
+        SceneManager.LoadScene("Main", LoadSceneMode.Single);
+        SceneManager.LoadScene("Server", LoadSceneMode.Additive);
+        SceneManager.LoadScene("Client", LoadSceneMode.Additive);
+    }
+}
