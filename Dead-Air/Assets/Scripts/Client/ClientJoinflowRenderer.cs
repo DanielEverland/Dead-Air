ffcd6f57e960f166c0d591caa3555c810783f081
Daniel Everland <danieleverland@gmail.com>
2018-05-16 14:05:43 +0200

    Added joining text on client

---
+++ Dead-Air/Assets/Scripts/Client/ClientJoinflowRenderer.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClientJoinflowRenderer : MonoBehaviour {
+
+    private void Start()
+    {
+        Client.Peer.OnReady += OnReady;
+    }
+    private void OnReady()
+    {
+        Destroy(gameObject);
+    }
+}
