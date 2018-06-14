d0aa95f8d002163c11e236f32fa7b6ca1e0d047c
Daniel Everland <danieleverland@gmail.com>
2018-06-14 19:39:18 +0200

    Added batch script for running server

---
+++ Solution/Core/Components/BatchmodeForwarder.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Components
+{
+    public class BatchmodeForwarder : MonoBehaviour
+    {
+        [SerializeField]
+        private string targetScene;
+        [SerializeField]
+        private LoadSceneMode loadMode = LoadSceneMode.Single;
+
+        private void Awake()
+        {
+            string commands = System.Environment.CommandLine;
+
+            if(commands.Contains("-batchmode") && commands.Contains("-nographics"))
+            {
+                SceneManager.LoadScene(targetScene, loadMode);
+            }
+        }
+    }
+}
---
+++ Unity/CopyToBuild/StartServer.bat
@@ -0,0 +1,1 @@
+"Dead Air.exe" -batchmode -nographics
